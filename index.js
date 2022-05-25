aa89f06e7f0633a2f1eb3cdfe1d6b1118d84efd7
Tamanna-akter <atamanna577@gmail.com>
2022-05-25 21:56:32 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+const { MongoClient, ServerApiVersion} = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q9lb9zo.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nf8hl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('connected');
+    }
+    finally{
+
+    }
+}
+    run().catch(console.dir);
 
 // ******************
 app.get("/", (req, res) => {
